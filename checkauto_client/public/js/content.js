98403a09bce40ac6c4b450661873a0327b379765
Thuận Trần <thuankg1752@gmail.com>
2020-11-22 22:21:08 +0700

    edit extension

--- checkauto_client/public/js/content.js
+++ checkauto_client/public/js/content.js
@@ -1,29 +1,6 @@
 function checkauto() {
   chrome.storage.sync.get(["userselected"], function (result) {
     $("input[type=text]").val(result["userselected"]);
-    $("button[type=submit]").click();
-  });
-}
-// function save() {
-//   var query = $("#formId").serialize();
-//   var url = "http://localhost/training-php/getdata.php";
-//   $.post(url, query, function (response) {
-//     alert(response);
-//   });
-// }
-
-function save() {
-  $.ajax({
-    query: $("#formId").serialize(),
-    type: "POST",
-    url:
-      "http://localhost/training-php/getdata.php?url=" +
-      encodeURIComponent(newURL),
-    data: { query, url: newURL },
-    success: function (data) {
-      $.post(data, function (respones) {
-        alert(respones);
-      });
-    },
+    $("input[type=submit]").click();
   });
 }
