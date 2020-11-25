d0cf0a1233620866349d92f8036062db7c89ea97
DESKTOP-8PLLBV6\thanh <phuhathanh247@gmail.com>
2020-11-25 14:43:33 +0700

    validate

--- Check_Auto/public/js/popup.js
+++ Check_Auto/public/js/popup.js
@@ -1,7 +1,31 @@
-$(document).ready(function(){
-    $("#send").click(function(){
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "http://localhost/local/?mssv="+$('#mssv').val(), true);
-        xhttp.send();
-    })
-})
+// $(document).ready(function() {
+//     $("#send").click(function() {
+//         var xhttp = new XMLHttpRequest();
+//         xhttp.open("GET", "http://localhost/local/?mssv=" + $('#mssv').val(), true);
+//         xhttp.send();
+//     });
+// })
+
+$(document).ready(function() {
+    $('#send').click(function(e) {
+        var isValid = true;
+        $('#mssv').each(function() {
+            if ($.trim($(this).val()) == '') {
+                isValid = false;
+                document.getElementById("notification").innerHTML = "Please input MSSV !!";
+            } else {
+                var xhttp = new XMLHttpRequest();
+                xhttp.open("GET", "http://localhost/local/?mssv=" + $('#mssv').val(), true);
+                xhttp.send();
+                document.getElementById("notification").innerHTML = "Successfully";
+                $("#notification").css({
+                    "color": "green"
+                });
+            }
+        });
+        if (isValid == false)
+            e.preventDefault();
+    });
+
+
+});
