eba7a42b92017e2fe988310856c6157bf51e80f4
tanhongit <phuongtan12357nguyen@gmail.com>
2020-12-01 13:34:18 +0700

    Test mssv users post up and auto-fill tabs

--- auto-check-user/public/js/content.js
+++ auto-check-user/public/js/content.js
@@ -1,50 +1,54 @@
+
 $(document).ready(function () {
-
-    $.ajax({    //create an ajax request to display.php
-        type: "GET",
-        url: "http://localhost/php-training/for-extension/auto-check-presence.php",
-        dataType: "html",   //expect html to be returned                
-        success: function (response) {
-            $("#responsecontainer").html(response);
-            //alert(response);
-        }
-    });
-
-    /* khởi tạo bộ lưu trữ nếu mở lần đầu*/
-    chrome.storage.sync.get(function () {
-        user = { "mssv": [] };
-        chrome.storage.sync.set({
-            "list": user
-        });
-    });
-    /*----------------*/
-
-    chrome.storage.sync.get(["list"], function (result) {
-        console.log(result['list']['mssv']);
-    });
-
-    $("#load").click(function () {
-        // console.log($('#mssv_user2').val());
-
-        var get_input_mssv;
-
-        var total_user_mssv = $('#total_user_mssv').val();
-        console.log(total_user_mssv);
-
-        //show data storage : https://chrome.google.com/webstore/detail/storage-area-explorer/ocfjjjjhkpapocigimmppepjgfdecjkb
-        //save list mssv from server file to chrome storage
-        chrome.storage.sync.get(["list"], function (result) {
-            for (var i = 1; i <= total_user_mssv; i++) {
-                if ($('#inlineCheckbox' + i).is(":checked") == true) {
-                    get_input_mssv = $('#mssv_user' + i).val();
-                    var get_list = result['list'];
-                    get_list['mssv'].push(get_input_mssv);
-                    chrome.storage.sync.set({
-                        "list": get_list
-                    });
-                }
-            }
-        });
-
-    });
+	chrome.runtime.onMessage.addListener(function (
+		request,
+		sender,
+		sendResqonse
+	) {
+		if (request.message === "start") {
+			load_presence();
+		}
+	});
 });
+
+function load_presence() {
+
+	var get_mssv_server;
+
+	chrome.storage.sync.get(["list_online"], function (result) {
+
+		//Attribute Starts With Selector: https://api.jquery.com/attribute-starts-with-selector/
+		var total_user_server = $('[id^="grvListStudents_lblStudentID_"]').length;
+
+		/*save list mssv from "main server online presence" to chrome storage*/
+		for (var i = 0; i < total_user_server; i++) {
+			if ($('#grvListStudents_lblStudentID_' + i)) {
+				get_mssv_server = $('#grvListStudents_lblStudentID_' + i).html();
+				var get_list = result['list_online'];
+				get_list['mssv_user'].push(get_mssv_server);
+				chrome.storage.sync.set({
+					"list_online": get_list
+				});
+			} else;
+		}
+
+		var list_user_server = result['list_online'];
+		console.log("so user tren server: " + total_user_server + ", " + list_user_server['mssv_user'].length);
+
+		/* Test mssv users post up and auto-fill tabs */
+		chrome.storage.sync.get(["list"], function (result_sync) {
+			var list_user_presenced = result_sync['list'];
+			console.log(list_user_presenced['mssv']);
+			console.log(list_user_server['mssv_user']);
+
+			for (let i = 0; i < list_user_server['mssv_user'].length; i++) {
+				if (jQuery.inArray(list_user_server['mssv_user'][i], list_user_presenced['mssv']) !== -1) {
+					$('#grvListStudents_txtVangCP_' + i).val(0);
+				} else {
+					$('#inputnumber' + i).val(5);
+					$('#grvListStudents_txtVangKP_' + i).val(5);
+				}
+			}
+		});
+	});
+}
--- auto-check-user/public/js/popup.js
+++ auto-check-user/public/js/popup.js
@@ -1,10 +1,78 @@
-//Sử lý sự kiện click
 $(document).ready(function () {
-	$('#load').click(function(){
-        // alert($('#save').html());
-		if ($('#load').html() == 'Load data') {
-			$('#load').html('Load Done');
-            $('#load').attr('disabled', "true");
+	//the first load database to extension 
+	$.ajax({    //create an ajax request to display.php
+		type: "GET",
+		url: "http://localhost/php-training/for-extension/auto-check-presence.php",
+		dataType: "html",   //expect html to be returned                
+		success: function (response) {
+			$("#responsecontainer").html(response);
+			//alert(response);
 		}
 	});
+	/*-------------------------------------------*/
+
+	/* re-initialize storage mssv every time it is loaded*/
+	chrome.storage.sync.get(function () {
+		user = { "mssv": [] };
+		chrome.storage.sync.set({
+			"list": user
+		});
+	});
+	chrome.storage.sync.get(["list"], function (result) {
+		console.log(result['list']['mssv']);
+	});
+	/*-------------------------------------------*/
+
+	// initialize storage for the list presence 
+	chrome.storage.sync.get(function () {
+		user = { "mssv_user": [] };
+		chrome.storage.sync.set({
+			"list_online": user
+		});
+	});
+	chrome.storage.sync.get(["list_online"], function (result) {
+		console.log(result['list_online']['mssv_user']);
+	});
+	/*-------------------------------------------*/
+
+	//Sử lý sự kiện click
+	$('#load').click(function () {
+		// alert($('#save').html());
+		if ($('#load').html() == 'Load data') {
+			$('#load').html('Load Done');
+			$('#load').attr('disabled', "true");
+		}
+	});
+	/*-------------------------------------------*/
+	$("#load").click(function () {
+		// console.log($('#mssv_user2').val());
+
+		var get_input_mssv;
+
+		var total_user_mssv = $('#total_user_mssv').val();
+		console.log(total_user_mssv);
+
+		//show data storage : https://chrome.google.com/webstore/detail/storage-area-explorer/ocfjjjjhkpapocigimmppepjgfdecjkb
+		//save list mssv from server file to chrome storage
+		chrome.storage.sync.get(["list"], function (result) {
+			for (var i = 1; i <= total_user_mssv; i++) {
+				if ($('#inlineCheckbox' + i).is(":checked") == true) {
+					get_input_mssv = $('#mssv_user' + i).val();
+					var get_list = result['list'];
+					get_list['mssv'].push(get_input_mssv);
+					chrome.storage.sync.set({
+						"list": get_list
+					});
+				}
+			}
+			console.log(result['list']);
+		});
+
+		//Use the chrome.tabs API to interact with the browser's tab system
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			var activeTab = tabs[0];
+			chrome.tabs.sendMessage(activeTab.id, { "message": "start" });
+		});
+	});
+	/*-------------------------------------------*/
 });
